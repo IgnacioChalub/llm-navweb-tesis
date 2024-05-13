606c7519c0b6756437771b53daf1e178a1bf02d9
beltranbulbarella <beltran.bulbarella@ing.austral.edu.ar>
2024-05-13 10:43:31 +0200

    feat: fixed fillRegisterScript.py for Serena name change

--- selenium-sandbox/SerenaScripts/fillRegisterScript.py
+++ selenium-sandbox/SerenaScripts/fillRegisterScript.py
@@ -1,17 +1,18 @@
-from GenericScript import generic_script
+from Serena import serena_script
+from Serena import Action
 
 def main():
     # Define the URL and actions for the form
     url = "http://localhost:3000/register"
     actions = [
-        ("INPUT", "example@email.com", "email-register-input"),
-        ("INPUT", "Beltran", "username-register-input"),
-        ("INPUT", "securepassword123", "password-register-input"),
-        ("CLICK-BUTTON", None, "submit-button-id")  # Ensure correct ID
+        Action("INPUT", "example@email.com", "email-register-input"),
+        Action("INPUT", "Beltran", "username-register-input"),
+        Action("INPUT", "securepassword123", "password-register-input"),
+        Action("CLICK-BUTTON", None, "submit-button-id")  # Ensure correct ID
     ]
 
     # Call the generic script function with the URL and actions
-    generic_script(url, actions)
+    serena_script(url, actions)
 
 if __name__ == "__main__":
     main()
