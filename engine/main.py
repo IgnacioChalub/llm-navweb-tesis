9f33a764829a0cec6c5fedb8ff60010faa6ac97b
Ignacio Chalub <ignaciochalub@Ignacios-MacBook-Pro.local>
2024-05-13 21:08:04 +0200

    added main func

--- engine/main.py
+++ engine/main.py
@@ -1,12 +1,14 @@
 from bicho.bicho import run_bicho
 from serena.serena import serena_script
 
-url = "http://localhost:3000/login"
-user_task = "Login with name Juan and password 123hola_123"
+def main():
+    url = "http://localhost:3000/login"
+    user_task = "Login with name Juan and password 123hola_123"
 
-actions = run_bicho(user_task)
-# for action in actions:
-#     print(action)
-serena_script(url, actions)
+    actions = run_bicho(user_task)
+    # for action in actions:
+    #     print(action)
+    serena_script(url, actions)
 
-
+if __name__ == "__main__":
+    main()
