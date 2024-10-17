6c85602f5eb651059cd0def8bc9d06aac0bd920c
ignaciochalub <ignaciochalub@gmail.com>
2024-10-17 09:52:41 -0300

    Revert "changes"
    This reverts commit 77f39e4476a757a3c9626f3dde4a00f324ea7aab.

--- engine/main.py
+++ engine/main.py
@@ -8,16 +8,14 @@ load_dotenv()
 
 def main():
     openai_api_key = os.getenv("OPENAI_API_KEY")
-    url = "http://localhost:3000/"
-    repo_path = "/Users/ignaciochalub/faculty/llm-navweb-tesis/next-sandbox/test-app/src"
-    # entry_file = "next-sandbox/test-app/src/app/(unAuthRoutes)/login/page.tsx"
-    entry_file = "next-sandbox/test-app/src/app/page.tsx"
+    url = "http://localhost:3000/login"
 
-    # user_task = get_text_from_speech(openai_api_key)
-    user_task = "Login with username ignacio and password ignacio and then Deposit $150"
+    user_task = get_text_from_speech(openai_api_key)
+    # user_task = "Login with username ignacio and password ignacio and then Deposit $150"
     print("User task:", user_task)
 
-    actions = run_bicho(openai_api_key, user_task, repo_path, entry_file)
+
+    actions = run_bicho(openai_api_key, user_task)
 
     serena_script(url, actions)
 
